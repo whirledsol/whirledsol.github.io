9c993e510d0ffc8995d66bea0f72450b72b7b250
whirledsol <astro491@gmail.com>
2024-12-12 15:19:37 -0500

    copyright

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,3 +2,6 @@ function emailLink(emailaddress, subject) {
 	subject = subject !== undefined && typeof (subject) == 'string' ? '?subject=' + encodeURIComponent(subject) : '';
 	parent.location = 'mai'+'l' + "t" + "o" + ':' + emailaddress + subject;
 }
+
+
+
