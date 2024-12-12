22d7b6aa51370d88a8bfbaa2398d2558470a14df
whirledsol <astro491@gmail.com>
2024-12-12 15:00:04 -0500

    first update after how long?

---
+++ assets/scripts.js
@@ -0,0 +1,4 @@
+function emailLink(emailaddress, subject) {
+	subject = subject !== undefined && typeof (subject) == 'string' ? '?subject=' + encodeURIComponent(subject) : '';
+	parent.location = 'mai'+'l' + "t" + "o" + ':' + emailaddress + subject;
+}
